0d2e7d35532e33bcc6367934a9cbd8218bdc1eac
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-07-03 20:06:39 +0900

    fix: logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,19 @@ LINE='##############################'
 MSGS='####### start action #########'
 
 LOCAL_HOST="http://localhost:1313"
+MAX_WAIT_TIME=600 # 5 min
 
 echo ${LINE}
 echo "${MSGS}"
 echo ${LINE}
 
 eval hugo server "${HUGO_OPTIONS}" > /dev/null &
-for i in $(seq 0 600); do # 5 min
+for i in $(seq 0 ${MAX_WAIT_TIME}); do # 5 min
     sleep 0.5
     IS_SERVER_RUNNING=$(curl -LI ${LOCAL_HOST} -o /dev/null -w '%{http_code}' -s)
     if [[ "${IS_SERVER_RUNNING}" == "200" ]]; then
-        break
+        eval muffet "${OPTIONS}" ${LOCAL_HOST} && exit 0 || exit 1
     fi
-    echo "error: time out $((i/2)) sec" && exit 1
 done
-eval muffet "${OPTIONS}" ${LOCAL_HOST}
+
+echo "error: time out $((${MAX_WAIT_TIME}/2)) sec" && exit 1
